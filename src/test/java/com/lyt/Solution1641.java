e73367d4afae51d1d0a29329850e576d3e27caef
liuyanting <liu.123456>
2020-12-08 13:39:00 +0800

    1247. 交换字符使得字符串相同

--- src/test/java/com/lyt/Solution1641.java
+++ src/test/java/com/lyt/Solution1641.java
@@ -1,60 +1,36 @@
 package com.lyt;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @Author: liuyanting
- * @Description: 1641. 统计字典序元音字符串的数目
- * @Date: 2020-12-7
+ * @Description: 1247. 交换字符使得字符串相同
+ * @Date: 2020-12-8
  **/
 public class Solution1641 {
     public static void main(String[] args) {
-        System.out.println(countVowelStrings2(3));
-        System.out.println(countVowelStrings2(3));
+        System.out.println(minimumSwap("xxyyxyxyxx", "xyyxyxxxyx"));
     }
 
-    public static int countVowelStrings(int n) {
-        if (n == 0) {
+    public static int minimumSwap(String s1, String s2) {
+        if (s1 == null || s1 == "" || s1.equals(s2)) {
             return 0;
         }
-        List<Integer> list = new ArrayList<>(1024);
-        list.add(5);
-        list.add(4);
-        list.add(3);
-        list.add(2);
-        list.add(1);
-        if (n == 1) {
-            return 5;
-        }
-        for (int i = 1; i < n; i++) {
-            int len = list.size();
-            for (int j = 0; j < len; j++) {
-                int m = list.get(j);
-                for (int k = 0; k < m - 1; k++) {
-                    list.add(k + 1);
+        int length = s1.length();
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        int dx = 0;
+        int dy = 0;
+        for (int i = 0; i < length; i++) {
+            if (arr1[i] != arr2[i]) {
+                if (arr1[i] == 'x') {
+                    dx++;
+                } else {
+                    dy++;
                 }
             }
         }
-        return list.size();
-    }
-
-    public static int countVowelStrings2(int n) {
-        if (n == 0) {
-            return 0;
+        if (dx % 2 != dy % 2) {
+            return -1;
         }
-        if (n == 1) {
-            return 5;
-        }
-        int[] arr = {1, 1, 1, 1, 1};
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < 5; j++) {
-                int m = arr[j];
-                for (int k = 0; k < j; k++) {
-                    arr[k] += m;
-                }
-            }
-        }
-        return arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+        return dx % 2 == 0 ? (dx + dy) / 2 : (dx + dy) / 2 + 1;
     }
 }
